c3f5494169326f8e52c9f266af4e08ac50357d5e
SemenchukOleg <olegsemenchuk79@gmail.com>
2023-04-04 17:08:46 +0300

    Fin m2 (2)

--- MODULE_2/LESSON_13/landing-project/landingproject_venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!F:\s\L13M02_final_task\landing-project\landingproject_venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
